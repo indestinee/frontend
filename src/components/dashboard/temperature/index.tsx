d4f350908ef500ae267c6846af4d30d91b712e16
Yaolin Chen <indestinee@gmail.com>
2022-05-31 14:20:38 +0800

    add temperature progress bar

--- src/components/dashboard/temperature/index.tsx
+++ src/components/dashboard/temperature/index.tsx
@@ -1,8 +1,47 @@
+import React, {useEffect, useState} from 'react';
+import ProgressBar from '../../progressBar';
+import './index.css';
+
 export default function Temperature() {
+  const [temperatures, setTemperatures] = useState<number[]>([]);
+
+  // TODO move to redux
+  const fetchTemperature = () => {
+    fetch('/temperature')
+        .then((res) => res.json())
+        .then(
+            (result) => {
+              setTemperatures(result.data);
+            },
+            (error) => {
+              const result = {'data': [10, 20, 55, 60, 65, 66, 73]};
+              setTemperatures(result.data);
+            },
+        );
+  };
+
+  useEffect(() => {
+    fetchTemperature();
+    setInterval(fetchTemperature, 2000);
+  }, []);
+
   return (
-    <div>
+    <>
       <h2>Temperatures</h2>
-      <p>1 2 3 4 5 6 7</p>
-    </div>
+      {
+        temperatures.map((val, index) => (
+          <div key={index} className='process-bar-block'>
+            <span>Sensor#{index}</span>&nbsp;
+            <ProgressBar
+              value={val}
+              minValue={0}
+              maxValue={100}
+              greenThreshold={{min: 0, max: 63}}
+              yellowThreshold={{min: 63, max: 68}}
+              redThreshold={{min: 68, max: 100}} />
+          </div>
+        ))
+      }
+    </>
   );
 }
