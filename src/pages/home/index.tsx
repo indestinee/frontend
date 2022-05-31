e6ee0d5b80c14b218d1eb3f37d87f5152995567f
Yaolin Chen <indestinee@gmail.com>
2022-05-31 14:48:18 +0800

    add wifi page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,9 @@
 import Temperature from '../../components/dashboard/temperature';
-import './index.css';
 
 export default function Home() {
   return (
     <>
-      <div className="container">
-        <Temperature />
-      </div>
+      <Temperature />
     </>
   );
 }
--- src/pages/wifi/index.tsx
+++ src/pages/wifi/index.tsx
@@ -1,9 +1,23 @@
+import wifiJson from './wifi.json';
+import './index.css';
+import QRCode from 'qrcode.react';
+
 export default function Wifi() {
   return (
-    <>
-      <main>
-        <h2>Welcome to the wifi page!</h2>
-      </main>
-    </>
+    <div>
+      {
+        Object.entries(wifiJson).map(
+            ([k, v]) => (
+              <div className="wifi" key={k}>
+                <p>SSID: {k}</p>
+                <p>PASSWORD: {v}</p>
+                <QRCode
+                  size={384}
+                  value={`WIFI:T:WPA;S:${k};P:${v};;`}
+                  renderAs="canvas" />,
+              </div>
+            ))
+      }
+    </div>
   );
 }
